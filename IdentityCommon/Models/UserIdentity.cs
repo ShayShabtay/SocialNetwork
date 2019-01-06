3e8da26511d3b3907b913d7916d902b9243dca18
ShayShabtay <shay161@gmail.com>
2019-01-06 18:33:35 +0200

    create db to identity service

---
+++ IdentityCommon/Models/UserIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityCommon.Models
+{
+    [DynamoDBTable("UsersIdentity")]
+    public class UserIdentity
+    {
+        [DynamoDBHashKey]
+        public string UserId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string WorkPlace { get; set; }
+    }
+}
