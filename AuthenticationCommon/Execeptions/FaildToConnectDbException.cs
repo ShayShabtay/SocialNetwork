0bb35ae1ed28b90d387e0513fcc9be09f56fd729
ShayShabtay <shay161@gmail.com>
2019-01-06 09:49:49 +0200

    register and manual login

---
+++ AuthenticationCommon/Execeptions/FaildToConnectDbException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationCommon.Execeptions
+{
+    public class FaildToConnectDbException : Exception
+    {
+        public FaildToConnectDbException()
+        {
+
+        }
+
+        public FaildToConnectDbException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
---
+++ AuthenticationServer/Controllers/TokenController.cs
@@ -0,0 +1,49 @@
+﻿using AuthenticationBL.Managers;
+using AuthenticationServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AuthenticationServer.Controllers
+{
+    public class TokenController : ApiController, ITokenApi
+    {
+        TokenManager _tokenManager;
+
+        public TokenController()
+        {
+            _tokenManager = new TokenManager();
+        }
+
+        public IHttpActionResult ValidateManualToken()
+        {
+            string token = "token";
+            bool isValidToken;
+            try
+            {
+                isValidToken = _tokenManager.ValidateManualToken(token);
+            }
+            catch (Exception)
+            {
+                return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Something went wrong"));
+            }
+
+            if(isValidToken)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        public IHttpActionResult ValidateFacebookToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
