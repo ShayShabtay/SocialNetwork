df5a7ff6f8ac43aff47d76e66b7f3a23979ff0d3
Arriel12 <39603265+Arriel12@users.noreply.github.com>
2019-01-21 15:04:03 +0200

    merge

--- UI/Controllers/SocialController.cs
+++ UI/Controllers/SocialController.cs
@@ -69,75 +69,11 @@ namespace UI.Controllers
         }
 
         // GET: Social/GetProfileAndFeeds/5
+        [HttpGet]
         public ActionResult GetUsersProfileAndFeeds()
         {
             return View();
         }
 
-        // GET: Social/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Social/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Social/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Social/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Social/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Social/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
